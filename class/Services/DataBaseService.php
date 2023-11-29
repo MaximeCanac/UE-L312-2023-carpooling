1b78160ec60db31a5c7e67454a4f5c2a5b1cc767
mxrlxw <121444190+nsa-dev@users.noreply.github.com>
2023-11-29 01:03:53 +0100

    feature-reservation: Update DataBaseService.php

--- class/Services/DataBaseService.php
+++ class/Services/DataBaseService.php
@@ -10,9 +10,9 @@ class DataBaseService
 {
     const HOST = '127.0.0.1';
     const PORT = '3306';
-    const DATABASE_NAME = 'carpooling';
+    const DATABASE_NAME = 'crpooling';
     const MYSQL_USER = 'root';
-    const MYSQL_PASSWORD = 'password';
+    const MYSQL_PASSWORD = '';
 
     private $connection;
 
@@ -261,6 +261,53 @@ class DataBaseService
         $isOk = $query->execute($data);
 
         return $isOk;
+    }
+
+
+    /**
+     * Create a Reservation
+     * 
+     * @param int $id_announcement : Announcement identifier
+     * @param int $id_user : User identifier
+     * @param DateTime $date : Current date
+     * @return bool
+     */
+    public function createReservation($id_announcement, $id_user, DateTime $date): bool {
+        $returnBool = false;
+
+        $data = array(
+            'id_announcement' => $id_announcement,
+            'id_user' => $id_user,
+            ':reservation_date' => $date->format(DateTime::W3C)
+        );
+
+        $sql = 'INSERT INTO reservations(id_announcement, id_user, date) VALUES (:id_announcement, :id_user, :reservation_date)';
+        $query = $this->connection->prepare($sql);
+        $returnBool = $query->execute($data);
+
+        return $returnBool;
+    }
+
+
+
+    /**
+     * Delete a Reservation
+     * 
+     * @param int $id_reservation : Reservation identifier
+     * @return bool
+     */
+    public function deleteReservation(int $id_reservation): bool {
+        $returnBool = false;
+
+        $data = array(
+            'id_reservation' => $id_reservation,
+        );
+
+        $sql = 'DELETE FROM reservations WHERE id_reservation = :id_reservation';
+        $query = $this->connection->prepare($sql);
+        $returnBool = $query->execute($data);
+
+        return $returnBool;
     }
 
 }
